08cdb527052e0969c54f19c1a8b3f5eb3c48c38c
Dario Nieddu <cy.rae@yahoo.com>
2024-02-01 21:09:23 +0100

    Initial Commit

---
+++ RingOfFire/RingOfFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingOfFireApp.swift
+//  RingOfFire
+//
+//  Created by Dario Nieddu on 01.02.24.
+//
+
+import SwiftUI
+
+@main
+struct RingOfFireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
